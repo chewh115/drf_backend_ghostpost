0cb913b1c433ea0e51d4f7a6b4b7e39e808c1de3
Chelsea White <chewh115@gmail.com>
2020-06-23 19:46:12 -0400

    Adds ghostpost model

--- ghostpost/models.py
+++ ghostpost/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class BoastOrRoast(models.Model):
+    boast_or_roast = models.BooleanField()
+    title = models.CharField(max_length=30)
+    post = models.CharField(max_length=280)
+    up_votes = models.IntegerField(default=0)
+    down_votes = models.IntegerField(default=0)
+    submit_time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+    
+    def score(self):
+        return self.up_votes - self.down_votes
