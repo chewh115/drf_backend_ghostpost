bfc29919bddc0cc49471b09bb90cbadd3a8cfb97
Chelsea White <chewh115@gmail.com>
2020-06-23 19:42:45 -0400

    Updates settings for drf, CORS and app

--- drf_ghostpost/settings.py
+++ drf_ghostpost/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jtcs^!1@dwd8iss1jbpl+)oo63x+tf!56rs-dhgz6%5ta!!=45'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,11 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'ghostpost'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -118,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
